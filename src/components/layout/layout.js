0dfbc678c3890fb174a4276bdb8f6e215cb2803f
박영준 <qkrdudwns98@soongsil.ac.kr>
2023-08-02 18:15:16 +0900

    git init

---
+++ src/components/layout/layout.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import Header from './header';
+import Footer from './footer';
+import Navbar from './nav';
+import Section from './section';
+import Aside from './aside';
+import './layout.css';
+
+function Layout(){
+    return(
+        <Container fluid>
+            <Row>
+                <Col><Header></Header></Col>
+            </Row>
+            <Row>
+                <Col><Navbar></Navbar></Col>
+            </Row>
+            <Row>
+                <Col><Section></Section></Col>
+                <Col xs={3}><Aside></Aside></Col>
+            </Row>
+            <Row>
+                <Col><Footer></Footer></Col>
+            </Row>
+        </Container>
+    )
+}
+export default Layout;
---
+++ src/components/layout/section.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './section.css';
+
+function Section(){
+    return(
+        <div className='container' id='section'>
+            <h2>Section</h2>
+            <h2>Section</h2>
+            <h2>Section</h2>
+            <h2>Section</h2>
+            <h2>Section</h2>
+            <h2>Section</h2>
+            <h2>Section</h2>
+        </div>
+    )
+}
+export default Section;
