011d4c2ba15b53c5cffd20c3129f1d27bb0451dc
박영준 <qkrdudwns98@soongsil.ac.kr>
2023-08-25 21:27:35 +0900

    [MODIFY] exact path

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Nav />
       <Router basename={process.env.PUBLIC_URL}>
         <Routes>
-          <Route path="/" element={<Home />}/>
+          <Route exact path="/" element={<Home />}/>
           <Route path="/enchant" element={<Enchant />}/>
         </Routes>
       </Router>
